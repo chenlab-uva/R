6cbb65ec44364ad1049635d6a905baa1bb0764a6
Zhennan-Z <pumpkinzzn@gmail.com>
2019-04-19 17:06:04 -0400

    Update

--- Ancestry_Inference.R
+++ Ancestry_Inference.R
@@ -1,207 +1,132 @@
-library(optparse)
-library(e1071)
-library(parallel)
-library(iterators)
-library(foreach)
-library(doParallel)
-library(ggplot2)
+print(date())
+
+# check packages
+required_packages <- c("e1071", "doParallel")
+error_msg <- paste("Please install the following packages:", paste(required_packages, collapse = ", "))
+if( !all(required_packages %in% installed.packages()[,'Package']) ) stop(error_msg)
+suppressMessages(library(e1071))
+suppressMessages(library(doParallel))
+
 # input setting
-option_list = list(
-  make_option(c("-f", "--file"), type="character", default=NULL, 
-              help="PC file", metavar="character"),
-  make_option(c("-p", "--phefile"), type="character", default=NULL,
-              help="population dataset file name", metavar="character"),
-  make_option(c("-n","--numberpc"), type="integer", default=10,
-              help="number of PCs to be selected [default %default]", metavar="integer"),
-  make_option(c("-k", "--kernel"), type="character", default="linear",
-              help = "Kernel Option [default \"%default\"]", metavar="character"),
-  make_option(c("--prefix"), type="character", default=NULL, 
-              help="output file name", metavar="character")
-); 
-opt_parser = OptionParser(option_list=option_list);
-opt = parse_args(opt_parser);
-if (is.null(opt$file) | is.null(opt$phe) | is.null(opt$prefix)){
-  print_help(opt_parser)
-  stop("PC file, population file and prefix  must be supplied", call.=FALSE)
-}
+args = commandArgs(TRUE)
+if( length(args) != 3 ) stop("please provide three arguments (pc file, population file and prefix)")
 
+pc_name <- args[1]
+phe_name <- args[2]
+prefix <- args[3]
 
-pc <- read.table(opt$file, header = TRUE)
-phe <- read.table(opt$phefile, header = TRUE)
-number <- opt$numberpc
+pc <- read.table(pc_name, header = TRUE)
+phe <- read.table(phe_name, header = TRUE)
 
+print(paste("Prepare the PC file and the reference file, starts at ",date(), sep = " "))
+print(paste("Apply 10 PCs to the analysis", sep = " "))
+print(paste("Choose linear", sep = " "))
 
-print(paste("Prepare the PC file and the reference file", sep=" "))
-print(paste("Apply",number,"PCs to the analysis", sep=" "))
-print(paste("Choose",opt$kernel,sep = " "))
-
-pop <- phe[, c("IID", "population")]
-train.data <- pc[pc$AFF == 1, c(2, 7:(number + 6))]
+pop <- phe[, c("IID", "Population")]
+train.data <- pc[pc$AFF == 1, c(2, 7:16)]
 train.phe <- merge(train.data, pop, by = "IID")
-test.data <- pc[pc$AFF == 2, c(1,2, 7:(number + 6))]
+test.data <- pc[pc$AFF == 2, c(1, 2, 7:16)]
 
-train.x <- train.phe[, !colnames(train.phe) %in% c("population", "IID")]
-train.y <- train.phe$population
+train.x <- train.phe[, !colnames(train.phe) %in% c("Population", "IID")]
+train.y <- train.phe[,"Population"]
 
 numCores <- detectCores()
 registerDoParallel(cores = round((numCores/2)))
 
-print(paste("Assign ",round((numCores/2))," cores for the grid search", sep = ""))
+print(paste("Assign ", round((numCores/2)), " cores for the grid search, starts at ", date(), sep = ""))
 print("Grid search with a wide range")
 
-if (opt$kernel == "linear") {
-  # For Linear
-  tuneresult <- foreach(cost = 2^(seq(-10, 10, by = 0.5)), .combine = rbind) %dopar% {
-    set.seed(123)
-    mod = tune(svm, train.x, as.factor(train.y), kernel = opt$kernel, 
-               cost = cost, probability = TRUE)
-    performance <- mod$performances[, c("error", "dispersion")]
-    data.frame(cost = cost, performance = performance)
-  }
-  best.setting <- tuneresult[tuneresult$performance.error == min(tuneresult$performance.error), ]
-  best.cost <- min(best.setting$cost)
-  
-  print(paste("The best cost is ", round(best.cost,6), " after the wide grid search", sep=""))
-  print("Grid search with a small range")
-  
-  more.cost <- 2^seq(log2(best.cost) - 0.5, log2(best.cost) + 0.5, by = 0.05)
-  tune.more <- foreach(cost = more.cost, .combine = rbind) %dopar% {
-    set.seed(123)
-    mod = tune(svm, train.x, as.factor(train.y), kernel = opt$kernel, 
-               cost = cost, probability = TRUE)
-    performance <- mod$performances[, c("error", "dispersion")]
-    data.frame(cost = cost, performance = performance)
-  }
-  best.setting <- tune.more[tune.more$performance.error == min(tune.more$performance.error), ]
-  best.cost <- min(best.setting$cost)
-  print(paste("The best cost is ", round(best.cost,6), " after the small grid search", sep=""))
-  # my final model
+# Linear Only
+tuneresult <- foreach(cost = 2^(seq(-10, 10, by = 0.5)), .combine = rbind) %dopar% {
   set.seed(123)
-  mymod <- svm(train.x, as.factor(train.y), cost = best.cost, kernel = opt$kernel,probability=TRUE)
-} else {
-   # For radial
-  tuneresult <- foreach(gamma = 2^(seq(-15, 3, by = 0.5)), .combine = rbind) %dopar% {
-    registerDoParallel(cores = 1)
-    foreach(cost = 2^(seq(-10, 10, by = 0.5)), .combine = rbind) %dopar% 
-    {
-      set.seed(123)
-      mod = tune(svm, train.x, as.factor(train.y), 
-                 kernel = opt$kernel, ranges = list(cost = cost, gamma = gamma), 
-                 probability = TRUE)
-      performance <- mod$performances[, c("error", "dispersion")]
-      data.frame(cost = cost, gamma = gamma, performance = performance)
-    }
-  }
-  # write.table(tuneresult,"radial_check_tuneresult_1.txt",quote=FALSE, row.names=FALSE)
-  best.setting <- tuneresult[tuneresult$performance.error == min(tuneresult$performance.error), ]
-  best.cost <- best.setting$cost[1]
-  best.gamma <- best.setting$gamma[1]
-  
-  print(paste("The best cost is ", round(best.cost,6), " after the wide grid search", sep=""))
-  print(paste("The best gamma is ", round(best.gamma,6), " after the wide grid search", sep=""))
-  print("Grid search with a small range")
-  more.cost <- 2^seq(log2(best.cost) - 0.5, log2(best.cost) + 0.5, by = 0.05)
-  more.gamma <- 2^seq(log2(best.gamma) - 0.5, log2(best.gamma) + 0.5, 
-                      by = 0.05)
- registerDoParallel(cores = round((numCores/2)))  
- tune.more <- foreach(gamma= more.gamma, .combine = rbind) %dopar% {
-   registerDoParallel(cores = 1)
-   foreach(cost = more.cost, .combine = rbind) %dopar% {
-      set.seed(123)
-      mod = tune(svm, train.x, as.factor(train.y), kernel = opt$kernel, 
-                 ranges = list(cost = cost, gamma = gamma), probability = TRUE)
-      performance <- mod$performances[, c("error", "dispersion")]
-      data.frame(cost = cost, gamma = gamma, performance = performance)
-    }
-  }
-  
-  best.setting <- tune.more[tune.more$performance.error == min(tune.more$performance.error), ]
-  best.cost <- best.setting$cost[1]
-  best.gamma <- best.setting$gamma[1]
-  print(paste("The best cost is ", round(best.cost,6), " after the wide grid search", sep=""))
-  print(paste("The best gamma is ", round(best.gamma,6), " after the wide grid search", sep=""))
-  set.seed(123)
-  mymod <- svm(train.x, as.factor(train.y), cost = best.cost, gamma = best.gamma, 
-               kernel = opt$kernel,probability=TRUE)
+  mod = tune(svm, train.x, as.factor(train.y), kernel = "linear", cost = cost, probability = TRUE, 
+             tunecontrol = tune.control(cross=5))
+  performance <- mod$performances[, c("error", "dispersion")]
+  data.frame(cost = cost, performance = performance)
 }
+best.setting <- tuneresult[tuneresult$performance.error == min(tuneresult$performance.error), ]
+best.cost <- min(best.setting$cost)
 
+print(paste("The best cost is ", round(best.cost, 6), " after the wide grid search", sep=""))
+print(paste("Grid search with a small range, starts at", date()))
 
-print("Predict the ancestry information")
+more.cost <- 2^seq(log2(best.cost) - 0.5, log2(best.cost) + 0.5, by = 0.05)
+tune.more <- foreach(cost = more.cost, .combine = rbind) %dopar% {
+  set.seed(123)
+  mod = tune(svm, train.x, as.factor(train.y), kernel = "linear", 
+             cost = cost, probability = TRUE, tunecontrol = tune.control(cross=5))
+  performance <- mod$performances[, c("error", "dispersion")]
+  data.frame(cost = cost, performance = performance)
+}
+best.setting <- tune.more[tune.more$performance.error == min(tune.more$performance.error), ]
+best.cost <- min(best.setting$cost)
+print(paste("Grid search with a small range, ends at", date()))
+print(paste("The best cost is ", round(best.cost, 6), " after the small grid search", sep=""))
+# final model
+set.seed(123)
+mymod <- svm(train.x, as.factor(train.y), cost = best.cost, kernel = "linear", probability=TRUE)
 
-pred.pop <- predict(mymod, test.data[, !colnames(test.data) %in%c("FID","IID")],probability=TRUE)
+print("Predict ancestry information")
+print(date())
+
+pred.pop <- predict(mymod, test.data[, !colnames(test.data) %in%c("FID","IID")], probability=TRUE)
 test.data$PRED <- pred.pop
 class.prob <- attr(pred.pop, "probabilities")
 
 
-print("Prepare the summary file")
+print(paste("Prepare the summary file, starts at", date()))
 
-maxn <- function(n) function(x) order(x,decreasing = TRUE)[n]
-fst_class.index <- apply(class.prob,1,maxn(1))
-test.data$FST <- colnames(class.prob)[fst_class.index]
-test.data$FST_PROB <- round(apply(class.prob,1,function(x) x[maxn(1)(x)]),3)
+orders <- t(apply(class.prob, 1, function(x) order(x,decreasing =T)))
+orders.class <- t(apply(orders, 1, function(x) colnames(class.prob)[x]))
+orders.probs <- t(sapply(1:nrow(class.prob), function(x) class.prob[x, orders[x,]]))
 
-sec_class.index <- apply(class.prob,1,maxn(2))
-test.data$SEC <- colnames(class.prob)[sec_class.index]
-test.data$SEC_PROB <- round(apply(class.prob,1,function(x) x[maxn(2)(x)]),3)
+# > 0.65
+check.cumsum <- t(apply(orders.probs, 1, cumsum))
+temp <- apply(check.cumsum, 1, function(x) which(x > 0.65)[1])
 
-third_class.index <- apply(class.prob,1,maxn(3))
-test.data$THIRD <- colnames(class.prob)[third_class.index]
-test.data$THIRD_PROB <- round(apply(class.prob,1,function(x) x[maxn(3)(x)]),3)
-
-fourth_class.index <- apply(class.prob,1,maxn(4))
-test.data$FOURTH <- colnames(class.prob)[fourth_class.index]
-test.data$FOURTH_PROB <- round(apply(class.prob,1,function(x) x[maxn(4)(x)]),3)
+PRED_CLASS <- sapply(1:length(temp), function (x) paste(orders.class[x, 1:as.numeric(temp[x])], collapse = ";"))
+PRED_PROB <- sapply(1:length(temp), function (x) paste(round(orders.probs[x, 1:as.numeric(temp[x])], 3), collapse = ";"))
 
 
-pre.number.part <- test.data[,c("FST_PROB","SEC_PROB","THIRD_PROB","FOURTH_PROB")]
-pre.pop.part <- test.data[,c("FST","SEC","THIRD","FOURTH")]
-# convert it to character
-pre.pop.part <- data.frame(lapply(pre.pop.part, as.character), stringsAsFactors=FALSE)
-# For each sample, find the first several classes with prob > 0.65
-check.number <- t(apply(pre.number.part,1,cumsum))
-temp <- apply(check.number, 1, function(x) which(x > 0.65)[1])
+pred.out <- cbind(test.data[, c("FID", "IID", "PC1", "PC2")], PRED_CLASS, PRED_PROB, orders.class[, 1:2], orders.probs[, 1:2])
+colnames(pred.out)[7:10] <- c("FST", "SEC", "FST_PROB", "SEC_PROB")
 
-#
-for (i in 1:length(temp)){
-  if (temp[i]==1) {
-    test.data$PRED_CLASS[i] <- pre.pop.part[i,1]
-    test.data$PRED_PROB[i] <- pre.number.part[i,1]
-  }
-  else {
-    test.data$PRED_CLASS[i] <- paste(pre.pop.part[i,1:as.numeric(temp[i])],collapse=";")
-    test.data$PRED_PROB[i] <- paste(pre.number.part[i,1:as.numeric(temp[i])],collapse=";")
-  }
-}
-
-
-pred.out <- test.data[,c("FID","IID","PC1","PC2","FST","FST_PROB","SEC","SEC_PROB","PRED_CLASS","PRED_PROB")]
-# 
-write.table(pred.out, paste(opt$prefix,"_",opt$kernel,"_",opt$numberpc,"PCs.txt", sep=""), sep = "\t", quote = FALSE, row.names = FALSE)
-print(paste("Results are saved to",paste(opt$prefix,"_",opt$kernel,"_",opt$numberpc,"PCs.txt", sep=""), sep=" "))
+print(paste("summary file is ready ", date()))
+write.table(pred.out, paste(prefix, "_linear_", 10, "PCs.txt", sep = ""), sep = "\t", quote = FALSE, row.names = FALSE)
+print(paste("Results are saved to", paste(prefix, "_", "linear", "_", 10, "PCs.txt", sep = ""), sep = " "))
 
 print("Generate ggplots")
-
-uncertain.index <- (pred.out$FST_PROB <= 0.65)
 pred.out$PRED_CLASS <- as.character(pred.out$PRED_CLASS)
-pred.out$PRED_CLASS[uncertain.index] <- "UNCERTAIN"
-
-Palette <- c("#0072B2", "#E69F00", "#CC79A7", "#009E73", "#F0E442", "#D55E00", 
-    "#56B4E9", "#556b2f", "#0000FF", "#999999")
+pred.out$PRED_CLASS[pred.out$FST_PROB <= 0.65] <- "UNCERTAIN"
+Palette <- c("#1F78B4","#33A02C","#E31A1C","#FF7F00","#6A3D9A","#B15928","#A6CEE3","#B2DF8A","#FB9A99","#FDBF6F","#CAB2D6","#FFFF99","999999")
 groups <- length(unique(pred.out$PRED_CLASS))
-cbPalette <- Palette[c(1:(groups - 1), 10)]
+cPalette <- Palette[c(1:(groups - 1), 10)]
 
-
-pdf(paste(opt$prefix, "_", opt$kernel, "_", opt$numberpc, "PCs.pdf", sep = ""))
-p <- ggplot(pred.out, aes(x = PC1, y = PC2, color = PRED_CLASS, label = IID))
-p <- p + geom_point() + scale_colour_manual(values = cbPalette)
-print(p)
-dev.off()
-
-# train.phe
-pdf("Reference.pdf")
-p <- ggplot(train.phe, aes(x = PC1, y = PC2, color = population, label = IID))
-p <- p + geom_point() + scale_colour_manual(values = cbPalette)
-print(p)
-dev.off()
-print(paste("Reference.pdf and ", opt$prefix, "_", opt$kernel, "_", opt$numberpc, "PCs.pdf are generated." , sep =""))
-print("Done!")
+if(!require("ggplot2")) {
+  pdf(paste(prefix, "_", "linear", "_", 10, "PCs.pdf", sep = ""))
+  plot(pred.out$PC1, pred.out$PC2, col = cPalette[pred.out$PRED_CLASS], xlab = "PC1", ylab = "PC2", pch = 16)
+  legend("topright", title = "Predicted", legend = unique(pred.out$PRED_CLASS), col = unique(cPalette[pred.out$PRED_CLASS]), pch = 16, cex = 0.5)
+  dev.off()
+  pdf("Reference.pdf")
+  plot(train.phe$PC1, train.phe$PC2, col = cPalette[train.phe$Population], xlab = "PC1", ylab = "PC2", pch = 16)
+  legend("topright", title = "Reported", legend = unique(train.phe$Population), 
+         col = unique(cPalette[train.phe$Population]), pch = 16, cex = 0.5)
+  print(paste("Reference.pdf and ", prefix, "_", "linear", "_", 10, "PCs.pdf are generated.", sep = ""))
+  dev.off()
+  print("Done")
+} else {
+  pdf(paste(prefix, "_", "linear", "_", 10, "PCs.pdf", sep = ""))
+  p <- ggplot(pred.out, aes(x = PC1, y = PC2, color = PRED_CLASS, label = IID))
+  p <- p + geom_point() + scale_colour_manual(values = cPalette)
+  print(p)
+  dev.off()
+  
+  pdf("Reference.pdf")
+  p <- ggplot(train.phe, aes(x = PC1, y = PC2, color = Population, label = IID))
+  p <- p + geom_point() + scale_colour_manual(values = cPalette)
+  print(p)
+  dev.off()
+  print(paste("Reference.pdf and ", prefix, "_", "linear", "_", 10, "PCs.pdf are generated.", sep = ""))
+  print("Done")
+}
+print(date())
